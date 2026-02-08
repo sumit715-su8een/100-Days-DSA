faec6b5d54e337fefad30c942bd60331166400fe
sumit715-su8een <246022279+sumit715-su8een@users.noreply.github.com>
2026-02-08 12:00:00 +0000

    Day 8: Add 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,17 @@
+﻿// Day 8 - Q1: Compute Power Using Recursion
+// Topic: Recursion, Math
+// Time: O(b), Space: O(b)
+
+#include <iostream>
+using namespace std;
+
+int power(int a, int b) {
+    if (b == 0) return 1;
+    return a * power(a, b-1);
+}
+
+int main() {
+    int a, b; cin >> a >> b;
+    cout << power(a, b) << endl;
+    return 0;
+}
