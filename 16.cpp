d185d1edb9386907df150afd81750f88ea6fa21f
sumit715-su8een <246022279+sumit715-su8een@users.noreply.github.com>
2026-02-16 12:00:00 +0000

    Day 16: Add 16.cpp

---
+++ 16.cpp
@@ -0,0 +1,20 @@
+﻿// Day 16 - Q1: Frequency Counter in Array
+// Topic: Array Count, HashMap Logic
+// Time: O(n), Space: O(k)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, arr[100], freq[1001] = {0};
+    cin >> n;
+    for (int i = 0; i < n; i++) { cin >> arr[i]; freq[arr[i]]++; }
+    for (int i = 0; i < n; i++) {
+        if (freq[arr[i]] > 0) {
+            cout << arr[i] << ":" << freq[arr[i]] << " ";
+            freq[arr[i]] = 0;
+        }
+    }
+    cout << endl;
+    return 0;
+}
