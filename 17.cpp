f8ea8c74cbccb346504995ccefbc67416a3636f4
sumit715-su8een <246022279+sumit715-su8een@users.noreply.github.com>
2026-02-17 12:00:00 +0000

    Day 17: Add 17.cpp

---
+++ 17.cpp
@@ -0,0 +1,20 @@
+﻿// Day 17 - Q1: Find Maximum and Minimum Element
+// Topic: Kadane, DP Intro
+// Time: O(n), Space: O(1)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, arr[100];
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    int mx = arr[0], mn = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > mx) mx = arr[i];
+        if (arr[i] < mn) mn = arr[i];
+    }
+    cout << "Max: " << mx << endl;
+    cout << "Min: " << mn << endl;
+    return 0;
+}
