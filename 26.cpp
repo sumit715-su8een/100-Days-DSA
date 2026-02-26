1c7dc5b606e0189806c4360b74f217e8efd78881
sumit715-su8een <246022279+sumit715-su8een@users.noreply.github.com>
2026-02-26 12:00:00 +0000

    Day 26: Add 26.cpp

---
+++ 26.cpp
@@ -0,0 +1,23 @@
+﻿// Day 26 - Q1: Doubly Linked List Insertion and Traversal
+// Topic: Doubly Linked List
+// Time: O(n), Space: O(n)
+
+#include <iostream>
+using namespace std;
+
+struct Node { int data; Node *next, *prev; };
+
+int main() {
+    int n, v; cin >> n;
+    Node *head = nullptr, *tail = nullptr;
+    for (int i = 0; i < n; i++) {
+        cin >> v;
+        Node* nd = new Node(); nd->data = v; nd->next = nullptr; nd->prev = nullptr;
+        if (!head) head = tail = nd;
+        else { tail->next = nd; nd->prev = tail; tail = nd; }
+    }
+    Node* cur = head;
+    while (cur) { cout << cur->data; if (cur->next) cout << " "; cur = cur->next; }
+    cout << endl;
+    return 0;
+}
